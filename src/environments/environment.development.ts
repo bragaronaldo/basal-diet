37286ac58198ca6aad3c774f145c7392b5fe151c
Ronaldo <ronaldo.tbj@gmail.com>
2024-07-17 20:29:25 -0300

    New design for food tables and header

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:8000',
-  // apiUrl: 'https://basal-diet-api-production.up.railway.app',
+  // apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://basal-diet-api-production.up.railway.app',
   nutritionixAPI: 'https://trackapi.nutritionix.com/'
 };
